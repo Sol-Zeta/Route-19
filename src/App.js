00f123944d611913e099dc9e513a0aafd6341304
María Soledad Pattoglio <mariasoledadpattoglio@Marias-MacBook-Pro.local>
2021-06-28 20:45:12 +0200

    add/google-ga

--- src/App.js
+++ src/App.js
@@ -4,8 +4,28 @@ import MapContainer from './components/MapContainer/MapContainer.js'
 import './components/MapContainer/MapContainer.css'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import Loading from './components/Loading/Loading.js'
-import Cookies from './components/Cookies/Cookies';
-import {RemoveScrollBar} from 'react-remove-scroll-bar';
+// --- REACT-GA Google Analytics --- ///
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+// import auth from './auth.ts'; // Sample authentication provider
+
+const trackingId = "200586696"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+// ReactGA.set({
+//   userId: auth.currentUserId(),
+//   // any data that is relevant to the user session
+//   // that you would like to track with google analytics
+// })
+
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 
 
 export default function App() {
@@ -13,7 +33,7 @@ export default function App() {
     return (
       <div className="App">
         {/* <RemoveScrollBar /> */}
-          <Router>
+          <Router history={history}>
             <Switch>
                 <Route exact path='/'>
                   <MapContainer/> 
