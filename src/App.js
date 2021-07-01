a8eda64294b1bc2433fb538d87284fd42b4c4b02
María Soledad Pattoglio <mariasoledadpattoglio@Marias-MacBook-Pro.local>
2021-07-01 20:39:35 +0200

    add/googletagmanager in index.html

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import MapContainer from './components/MapContainer/MapContainer.js'
 import './components/MapContainer/MapContainer.css'
@@ -9,31 +9,36 @@ import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
 // import auth from './auth.ts'; // Sample authentication provider
 
-const trackingId = "200586696"; // Replace with your Google Analytics tracking ID
-ReactGA.initialize(trackingId);
+
 // ReactGA.set({
 //   userId: auth.currentUserId(),
 //   // any data that is relevant to the user session
 //   // that you would like to track with google analytics
 // })
 
-ReactGA.pageview(window.location.pathname + window.location.search);
 
-const history = createBrowserHistory();
+// const history = createBrowserHistory();
 
-// Initialize google analytics page view tracking
-history.listen(location => {
-  ReactGA.set({ page: location.pathname }); // Update the user's current page
-  ReactGA.pageview(location.pathname); // Record a pageview for the given page
-});
+// // Initialize google analytics page view tracking
+// history.listen(location => {
+//   ReactGA.set({ page: location.pathname }); // Update the user's current page
+//   ReactGA.pageview(location.pathname); // Record a pageview for the given page
+// });
 
 
 export default function App() {
 
+  // const trackingId = "200586696"; // Replace with your Google Analytics tracking ID
+  useEffect(() => {
+    ReactGA.initialize('200586696');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+
+  
     return (
       <div className="App">
         {/* <RemoveScrollBar /> */}
-          <Router history={history}>
+          <Router>
             <Switch>
                 <Route exact path='/'>
                   <MapContainer/> 
