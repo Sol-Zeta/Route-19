9853f3a3fc46d82cc61acf1a91df19ee78348209
Sol <mspattoglio@gmail.com>
2021-07-01 16:50:11 +0200

    fixing ga

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import MapContainer from './components/MapContainer/MapContainer.js'
 import './components/MapContainer/MapContainer.css'
@@ -9,13 +9,14 @@ import ReactGA from 'react-ga';
 import { createBrowserHistory } from 'history';
 // import auth from './auth.ts'; // Sample authentication provider
 
-const trackingId = "200586696"; // Replace with your Google Analytics tracking ID
-ReactGA.initialize(trackingId);
-// ReactGA.set({
-//   userId: auth.currentUserId(),
-//   // any data that is relevant to the user session
-//   // that you would like to track with google analytics
-// })
+
+  const trackingId = "200586696"; // Replace with your Google Analytics tracking ID
+  ReactGA.initialize(trackingId, {debug: true});
+  // ReactGA.set({
+  //   userId: auth.currentUserId(),
+  //   // any data that is relevant to the user session
+  //   // that you would like to track with google analytics
+  // })
 
 ReactGA.pageview(window.location.pathname + window.location.search);
 
